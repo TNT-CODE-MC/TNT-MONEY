7cf1cedb05ed90c827f6405ac82c7932d9258245
GuteeQ <wiktor.gut08@icloud.com>
2022-05-23 22:14:48 +0200

    New file - LineTokenizer.java (1/2500)

---
+++ com/GuTeK/activation/registries/LineTokenizer.java
@@ -0,0 +1,22 @@
+/*
+    Author: .GuTeK <wiktor@tntcode.pl>
+    Project: TNT-MONEY [https://tntcode.pl/plugins/TNT-MONEY]
+    Resources: 1/2500
+    Date: 23.05.2022
+    Contact Discord: .GuTeK#5326
+    Contact e-mail: wiktor@tntcode.pl
+    Our Websites: https://tntnetwork.pl / https://tntcode.pl
+    ⓒ 2022 by .GuTeK | ALL RIGHTS RESERVED |
+*/
+
+package GuTeK.activation.registries;
+
+import java.util.Vector;
+
+class LineTokenizer {
+    private int currentPosition;
+    private int maxPosition;
+    private String str;
+    private Vector stack;
+    private static final String singles = "=";
+}
