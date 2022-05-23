9f3dad7f93b2dd2ed1e4d101cf6c4b1f6a9c0aba
GuteeQ <wiktor.gut08@icloud.com>
2022-05-23 22:27:45 +0200

    Update file - LineTokenizer.java (1/2500)

--- com/GuTeK/activation/registries/LineTokenizer.java
+++ com/GuTeK/activation/registries/LineTokenizer.java
@@ -1,7 +1,7 @@
 /*
     Author: .GuTeK <wiktor@tntcode.pl>
     Project: TNT-MONEY [https://tntcode.pl/plugins/TNT-MONEY]
-    Resources: 1/2500
+    Resources: 1/1600
     Date: 23.05.2022
     Contact Discord: .GuTeK#5326
     Contact e-mail: wiktor@tntcode.pl
@@ -19,4 +19,32 @@ class LineTokenizer {
     private String str;
     private Vector stack;
     private static final String singles = "=";
+
+    public LineTokenizer(final String str) {
+        this.stack = new Vector();
+        this.currentPosition = 0;
+        this.str = str;
+        this.maxPosition = str.length();
+    }
+
+    private void skipWhiteSpace() {
+        while (this.currentPosition < this.maxPosition && Character.isWhitespace(this.str.charAt(this.currentPosition))) {
+            ++this.currentPosition;
+        }
+    }
+
+    public boolean hasMoreTokens() {
+        if (this.stack.size() > 0) {
+            return true;
+        }
+        this.skipWhiteSpace();
+        return this.currentPosition < this.maxPosition;
+    }
+
+    public String nextToken() {
+        final int size = this.stack.size();
+        if (size > 0) {
+            final String t = this.stack.elementAt(size - 1);
+        }
+    }
 }
